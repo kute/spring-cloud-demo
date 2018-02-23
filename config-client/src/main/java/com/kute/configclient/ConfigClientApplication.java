e4a1f029003d6ffc99b6168bb1c28d6c2e61b151
bailong03 <bailong03@meituan.com>
2018-02-23 19:55:56 +0800

    config update

--- config-client/src/main/java/com/kute/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/kute/configclient/ConfigClientApplication.java
@@ -1,25 +1,22 @@
 package com.kute.configclient;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
 
-    @Value("${current.user}")
-    private String user;
+    @GetMapping("/")
+    public String home() {
+        return "spring-cloud-config-client-home-api";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
-
-	@GetMapping("/getuser")
-	public String getUser() {
-	    return user;
-    }
 
 }
